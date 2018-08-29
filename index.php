059ab14116c3638c79c815e43b1aeceb82e129a1
jordy villavicencio <jordyhvillavicencio@gmail.com>
2018-08-29 13:57:55 -0500

    cambioss

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require 'conexion.php';
-$fotos_por_pagina=9;
+$fotos_por_pagina=6;
 $pagina_actual=(isset($_GET['p'])?(int)$_GET['p']:1);
 $inicio=($pagina_actual>1)?$pagina_actual*$fotos_por_pagina-$fotos_por_pagina:0;
 $sql="SELECT SQL_CALC_FOUND_ROWS * FROM fotos LIMIT $inicio, $fotos_por_pagina";
